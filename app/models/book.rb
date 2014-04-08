a27a5e426ea9da33589615db4547f7bfd956bfda
Lutz Biedinger <lutz.biedigner@gmail.com>
2014-04-08 15:21:16 +0100

    add page model and relationship with book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveFedora::Base
+  has_many :pages, :property=> :is_part_of
+
   has_metadata 'descMetadata', type: BookMetadata
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
